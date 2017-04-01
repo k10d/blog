f9fe2a2693a8466e8492d01182630883460e97f0
Kristen Dreifuerst <kdreifuerst@uwalumni.com>
2017-03-31 20:14:49 -0500

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Kristen Dreifuerst'
+SITENAME = "Kristen's Notes"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Central'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+LINKS = (
+#         ('Title', 'http://example.org/'),
+)
+
+SOCIAL = (
+    ('Github', 'http://github.com/k10d'),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
